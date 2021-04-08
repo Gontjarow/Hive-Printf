c62ca01196493ccc49db695dedb3a7cf45fed260
ngontjar <54755292+Gontjarow@users.noreply.github.com>
2021-04-08 08:40:19 +0000

    Header norm fix.

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ngontjar <ngontjar@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 18:57:36 by ngontjar          #+#    #+#             */
-/*   Updated: 2021/04/05 20:58:27 by ngontjar         ###   ########.fr       */
+/*   Updated: 2021/04/08 08:35:13 by ngontjar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@
 ** z = zero padding
 */
 
-typedef struct	s_data
+typedef struct s_data
 {
 	va_list	ap;
 	int		bit;
@@ -52,64 +52,64 @@ typedef struct	s_data
 	size_t	p;
 	size_t	w;
 	size_t	z;
-}				t_data;
+}	t_data;
 
 /*
 ** Main
 */
 
-int				ft_printf(const char *format, ...);
-char			parse_format(const char *format, t_data *flag);
+int		ft_printf(const char *format, ...);
+char	parse_format(const char *format, t_data *flag);
 
 /*
 ** Parser helpers
 */
 
-char			*strany(const char *string, char *specifiers);
-char			*strany_skip(const char *string, char *specifiers);
-char			*strchr_skip(const char *string, char specifier);
+char	*strany(const char *string, char *specifiers);
+char	*strany_skip(const char *string, char *specifiers);
+char	*strchr_skip(const char *string, char specifier);
 
 /*
 ** Conversion: s
 */
 
-void			output_str(char *arg, t_data *flag);
+void	output_str(char *arg, t_data *flag);
 
 /*
 ** Conversion: di c
 */
 
-void			output_int(long long arg, t_data *flag);
-void			output_char(unsigned char arg, t_data *flag);
+void	output_int(long long arg, t_data *flag);
+void	output_char(unsigned char arg, t_data *flag);
 
 /*
 ** Conversion: Xx u o
 */
 
-void			output_uint(unsigned long long arg, t_data *flag);
-void			output_unsigned(unsigned long long arg, t_data *flag);
-void			output_octal(unsigned long long arg, t_data *flag);
+void	output_uint(unsigned long long arg, t_data *flag);
+void	output_unsigned(unsigned long long arg, t_data *flag);
+void	output_octal(unsigned long long arg, t_data *flag);
 
 /*
 ** Conversion: p
 */
 
-void			output_pointer(void *arg, t_data *flag);
+void	output_pointer(void *arg, t_data *flag);
 
 /*
 ** Conversion: f
 */
 
-void			output_double(long double arg, t_data *flag);
+void	output_double(long double arg, t_data *flag);
 
 /*
 ** Output helpers
 */
 
-size_t			putstr_case(const char *str, int mode);
-size_t			putstrn_case(const char *str, size_t n, int mode);
-void			width_padder(int w, const char c, t_data *flag);
-char			*ft_utoa(unsigned long long number);
-char			*ft_utoa_base(unsigned long long number, int base);
+size_t	putstr_case(const char *str, int mode);
+size_t	putstrn_case(const char *str, size_t n, int mode);
+void	width_padder(int w, const char c, t_data *flag);
+char	*ft_utoa(unsigned long long number);
+char	*ft_utoa_base(unsigned long long number, int base);
 
 #endif
